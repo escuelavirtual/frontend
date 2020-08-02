3de90a8fefa2094168acf15665b1d460618470a2
jonathan0126 <39790533+jonathan0126@users.noreply.github.com>
2020-08-01 22:18:18 -0500

    agregado ejemplo de context api y reducer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
+import AuthState from "../contexts/authState";
 import './style.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Este es el codigo del proyecto de <strong>Escuela virtual</strong> por ahora nos encontamos trabajando en la app
-        </p>
-        <code>
-          Hola mundo
-        </code>
-        <a
-          className="escuelaDigitalGitHub"
-          href="https://github.com/escuelavirtual"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ver en github
-        </a>
-      </header>
-    </div>
+    <AuthState>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Este es el codigo del proyecto de <strong>Escuela virtual</strong> por ahora nos encontamos trabajando en la app
+          </p>
+          <code>
+            Hola mundo
+          </code>
+          <a
+            className="escuelaDigitalGitHub"
+            href="https://github.com/escuelavirtual"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Ver en github
+          </a>
+        </header>
+      </div>
+    </AuthState>
   );
 }
 
