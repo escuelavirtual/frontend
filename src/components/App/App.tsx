7642f367644b79a3e0c5e1f92493506c20a54a23
Jheyson Saavedra <66929614+JheysonSaavedra@users.noreply.github.com>
2020-08-04 14:45:54 -0500

    arreglando unos commits

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+<<<<<<< HEAD
 import AuthState from "../contexts/authState";
+=======
+>>>>>>> parent of 4970bf8... Merge branch 'dev' of github.com:escuelavirtual/frontend into dev
 import './style.scss';
 
 function App() {
   return (
+<<<<<<< HEAD
     <AuthState>
       <div className="App">
         <header className="App-header">
@@ -24,6 +28,26 @@ function App() {
         </header>
       </div>
     </AuthState>
+=======
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Este es el codigo del proyecto de <strong>Escuela virtual</strong> por ahora nos encontamos trabajando en la app
+        </p>
+        <code>
+          Hola mundo
+        </code>
+        <a
+          className="escuelaDigitalGitHub"
+          href="https://github.com/escuelavirtual"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ver en github
+        </a>
+      </header>
+    </div>
+>>>>>>> parent of 4970bf8... Merge branch 'dev' of github.com:escuelavirtual/frontend into dev
   );
 }
 
