b72efa08f6811215da4d011b516344380f854b42
Jheyson Saavedra <66929614+JheysonSaavedra@users.noreply.github.com>
2020-08-06 14:15:42 -0500

    limpiando errores y corrijiendo todo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,53 +1,13 @@
-import React from 'react';
-<<<<<<< HEAD
-import AuthState from "../contexts/authState";
-=======
->>>>>>> parent of 4970bf8... Merge branch 'dev' of github.com:escuelavirtual/frontend into dev
-import './style.scss';
+import React from "react";
+import "./style.scss";
 
-function App() {
+function App(): JSX.Element {
   return (
-<<<<<<< HEAD
-    <AuthState>
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Este es el codigo del proyecto de <strong>Escuela virtual</strong> por ahora nos encontamos trabajando en la app
-          </p>
-          <code>
-            Hola mundo
-          </code>
-          <a
-            className="escuelaDigitalGitHub"
-            href="https://github.com/escuelavirtual"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Ver en github
-          </a>
-        </header>
-      </div>
-    </AuthState>
-=======
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Este es el codigo del proyecto de <strong>Escuela virtual</strong> por ahora nos encontamos trabajando en la app
-        </p>
-        <code>
-          Hola mundo
-        </code>
-        <a
-          className="escuelaDigitalGitHub"
-          href="https://github.com/escuelavirtual"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ver en github
-        </a>
-      </header>
+    <div>
+      <h1>
+        esta pagina esta en desarrollo si vez este mensaje es que todo esta bien
+      </h1>
     </div>
->>>>>>> parent of 4970bf8... Merge branch 'dev' of github.com:escuelavirtual/frontend into dev
   );
 }
 
--- src/config/axios.tsx
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const clienteAxios = axios.create({
-  baseURL: "https://localhost:4000/api",
-});
-
-export default clienteAxios;
