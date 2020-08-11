6d4f140d3039c69c719b8959c867be0cb3f7f611
Jheyson Saavedra <66929614+JheysonSaavedra@users.noreply.github.com>
2020-08-11 12:34:59 -0500

    mejorando los componentes y aplicandoles props (navigation components)

--- src/components/navigation/NavAppInfo.tsx
+++ src/components/navigation/NavAppInfo.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.scss";
 
 const NavAppInfo = (props: any) => {
+<<<<<<< HEAD
   return (
     <>
       <div className="NavAppInfo">
@@ -14,6 +15,20 @@ const NavAppInfo = (props: any) => {
     </>
   );
 };
+=======
+		return (
+				<>
+					<div className="NavAppInfo">
+						<figure></figure>
+						
+							<span>{props.AppName}</span>
+							<span>{props.AppVersion}</span>
+						
+					</div>
+				</>
+			)
+	}
+>>>>>>> 87ffbcb58670e0f288254d1ca9f497ef4c7937b6
 
 type props = { AppName: string; AppVersion: string | number };
 
--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -6,11 +6,20 @@ import "./style.scss";
 
 function Navigation(props: any): JSX.Element {
   return (
+<<<<<<< HEAD
     <>
       <div id="Navigation">
         <nav>
           <div>
             <NavAppInfo appName={props.appName} appVersion={props.appVersion} />
+=======
+    <div className="navigation">
+      <nav>
+
+        <div className="navLeft">
+          <div className="navigationNavLogo">
+            <NavAppInfo/>
+>>>>>>> 87ffbcb58670e0f288254d1ca9f497ef4c7937b6
           </div>
           <div>
             <Searcher placeholder={props.placeholder}/>
@@ -18,9 +27,16 @@ function Navigation(props: any): JSX.Element {
           <div className="navigationItems">
             <NavItems items={props.items} />
           </div>
+<<<<<<< HEAD
         </nav>
       </div>
     </>
+=======
+
+        </div>
+      </nav>
+    </div>
+>>>>>>> 87ffbcb58670e0f288254d1ca9f497ef4c7937b6
   );
 }
 
