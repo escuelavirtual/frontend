e7be2a1ac70684494d2264fa75d235903a0d7805
Jheyson Saavedra <66929614+JheysonSaavedra@users.noreply.github.com>
2020-08-15 22:47:17 -0500

    mejorando la importacion de la rutas

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Home from "../../routes/home/index";
-import Login from "../../routes/login";
+import "./style.scss";
+
+const Home = React.lazy(() => import("../../routes/home/index"));
+const Login = React.lazy(() => import("../../routes/login/index"));
 
 function App(): JSX.Element {
   return (
-    <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-    </Router>
-  )
+    <Suspense
+      fallback={
+        <div className="loading">
+          <span>Cargando ...</span>
+        </div>
+      }>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+      </Router>
+    </Suspense>
+  );
 }
 
 export default App;
