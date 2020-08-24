4999063307573c755b0e11743d4227b56d8efb0d
CoffeJeanCode <ortizmurciajeanpierre@gmail.com>
2020-08-24 02:27:44 +0000

    cambiando estructura de renderizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-// Importacion de Pages
-const Home = lazy(() => import("./pages/home/index"));
-const Login = lazy(() => import("./pages/login/index"));
-const PageNotFound = lazy(() => import("./pages/Page404/index"));
+import { Navigation } from "./components/organisms/organisms";
+import { Home, Login, PageNotFound } from "./pages/pages";
 
 export default function App(): JSX.Element {
   return (
-    <Suspense fallback={<h1 className="loader">Loading...</h1>}>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route component={PageNotFound} />
-        </Switch>
-      </Router>
-    </Suspense>
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </Router>
   );
 }
---
+++ src/pages/pages.js
@@ -0,0 +1,3 @@
+export { default as Home } from "./home/index";
+export { default as Login } from "./login/index";
+export { default as PageNotFound } from "./Page404/index";
