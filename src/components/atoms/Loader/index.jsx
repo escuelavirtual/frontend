f5d292516ddae593b194166e03cbd7874cc249f1
CoffeJeanCode <ortizmurciajeanpierre@gmail.com>
2020-09-11 17:07:09 +0000

    fix(component): extracting styles of Loader with styled components

--- src/components/atoms/Loader/index.jsx
+++ src/components/atoms/Loader/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-
+import { BackLoader, StyledLoader, LoaderBar, Ball } from "./styles";
 export default function Loader() {
   return (
-    <div className="back-loader">
-      <div className="loader">
-        <div className="loader__bar"></div>
-        <div className="loader__bar"></div>
-        <div className="loader__bar"></div>
-        <div className="loader__bar"></div>
-        <div className="loader__bar"></div>
-        <div className="loader__ball"></div>
-      </div>
-    </div>
+    <BackLoader>
+      <StyledLoader>
+        <LoaderBar />
+        <LoaderBar />
+        <LoaderBar />
+        <LoaderBar />
+        <LoaderBar />
+        <Ball />
+      </StyledLoader>
+    </BackLoader>
   );
 }
