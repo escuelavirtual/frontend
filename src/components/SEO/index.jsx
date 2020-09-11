2732e532446ea0909cad3979114f0847961387b8
CoffeJeanCode <ortizmurciajeanpierre@gmail.com>
2020-09-11 16:45:52 +0000

    fix(extension): change ts extension to js in SEO and pages

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Helmet from "react-helmet";
 
-export default function SEO(props: any): JSX.Element {
+export default function SEO(props) {
   return (
     <Helmet>
       <meta charSet="utf-8" />
